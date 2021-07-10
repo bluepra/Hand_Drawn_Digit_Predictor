70f0c765e68d3a0ea2748d110486782f0f23bc1f
Prannav Arora <71447892+bluepra@users.noreply.github.com>
2021-07-10 10:58:41 -0500

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,8 +1,11 @@
 import pygame
 import torch
 from model import Net, model_save_path
+from PIL import Image
+import numpy as np
 
 # Model
+print(model_save_path)
 model = torch.load(model_save_path)
  
 # Colors
@@ -23,9 +26,24 @@ clock = pygame.time.Clock()
 
 # Use the PIL.Image class to reshape and convert image to ndarray and then to tensor
 def resize_image(path):
-    pass
+    # Open image 
+    image = Image.open(path).convert('L')
+
+    # Resize to (28,28)
+    new_width = 28
+    new_size = (new_width, new_width)
+    image = image.resize(new_size)
+    image.save('resized_gray.png')
+
+    arr = np.array(image.getdata())
+    arr = np.reshape(arr, new_size) / 255
+    # print(arr)
+
+    t = torch.as_tensor(arr)
+    t = torch.reshape(t, (1, 1, new_width, new_width))
+
+    return t
     
- 
 while run:
     left_pressed, middle_pressed, right_pressed = pygame.mouse.get_pressed()
     
@@ -44,9 +62,12 @@ while run:
             if event.key == pygame.K_RETURN:
                 image_path = 'user_drawn_image.png'
                 pygame.image.save(screen, image_path)
-                # image = resize_image(image_path)
-                # pred = model(image)
-                print('Mr.Digit predicts your drawing to be a')
+                image = resize_image(image_path)
+                # print(image)
+                pred = model(image.float())
+                # print(pred)
+                pred = round(pred.item())
+                print('Your drawing is a', pred)
 
                 # screen.fill(BLACK)
             if event.key == pygame.K_c:
@@ -56,7 +77,7 @@ while run:
     pygame.display.flip()
  
     # 60 frames per second
-    clock.tick(60)
+    clock.tick(20)
  
 # Close the window and quit.
 pygame.quit()
